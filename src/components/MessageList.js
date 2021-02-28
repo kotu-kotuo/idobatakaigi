97350eade5b78efe71ca8306f9e8a7efd6739b6d
kotu-kotuo <ttn-0223@mineo.jp>
2021-02-28 23:42:29 +0900

    apply grid row

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,8 +1,17 @@
 import { Divider } from "@material-ui/core";
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import classes from "*.module.css";
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: 1,
+  },
+});
 
 const MessageList = () => {
-return <div>MessageList</div>
-}
+  const classes = useStyles();
+  return <div className={classes.root}>MessageList</div>;
+};
 
-export default MessageList
+export default MessageList;
