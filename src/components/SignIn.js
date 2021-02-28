c9a951ef2254b20649fc3c165a8306a6b9e8de80
kotu-kotuo <ttn-0223@mineo.jp>
2021-02-28 17:58:12 +0900

    save-name-by-enter

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -67,6 +67,12 @@ export default function SignIn({setName}) {
             name="name"
             autoFocus
             onChange={(e) => setString(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setName = (e.target.value)
+                e.preventDefault()
+              }
+            }}
           />
 
           <Button
