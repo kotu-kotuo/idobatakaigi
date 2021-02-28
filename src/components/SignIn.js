2430b0ef8a2cd056765e54fd961ddbb11af2cd63
kotu-kotuo <ttn-0223@mineo.jp>
2021-02-28 17:38:27 +0900

    set name by clicking button

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -12,7 +12,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/" target="_blank" rel="noopener">
+      <Link color="inherit" href="https://material-ui.com/" target="_blank" >
         chat-app
       </Link>
     </Typography>
@@ -41,6 +41,13 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignIn({setName}) {
   const classes = useStyles();
+  const [disabled, setDisabled] = useState(true)
+  const [string, setString] = useState("")
+
+  useEffect(() => {
+    const disabled = string === ""
+    setDisabled(disabled)
+  },[string])
 
   return (
     <Container component="main" maxWidth="xs">
@@ -59,6 +66,7 @@ export default function SignIn({setName}) {
             label="ニックネーム"
             name="name"
             autoFocus
+            onChange={(e) => setString(e.target.value)}
           />
 
           <Button
@@ -67,6 +75,7 @@ export default function SignIn({setName}) {
             variant="contained"
             color="primary"
             className={classes.submit}
+            disabled={disabled}
           >
             始める
           </Button>
